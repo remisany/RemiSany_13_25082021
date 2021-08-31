f7fbbaca79d0029e5ea393cd72b136459e57dcf1
remisany <remi.d.sany@gmail.com>
2021-08-31 16:48:46 +0200

    Passage en composant fonction

--- argent-bank/src/pages/HomePage.jsx
+++ argent-bank/src/pages/HomePage.jsx
@@ -1,19 +1,14 @@
-//React
-import { Component } from "react"
-
 //Components
 import Hero from "../components/Hero"
 import Features from "../components/Features"
 
-class HomePage extends Component {
-    render () {
-        return (
-            <main>
-                <Hero />
-                <Features />
-            </main>
-        )
-    }
+function HomePage () {
+    return (
+        <main>
+            <Hero />
+            <Features />
+        </main>
+    )
 }
 
 export default HomePage
