c8daa80e9278acc164b68d6e9be6b654fb50daf1
remisany <remi.d.sany@gmail.com>
2021-08-31 16:31:40 +0200

    Ajour provider

--- argent-bank/src/index.jsx
+++ argent-bank/src/index.jsx
@@ -2,10 +2,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { Provider } from "react-redux"
+
+//Store
+import store from "./utils/store"
 
 //Pages
 import HomePage from "./pages/HomePage"
 import SignInPage from "./pages/SignInPage"
+import UserPage from "./pages/UserPage"
 
 //Components
 import Header from "./components/Header"
@@ -15,20 +20,26 @@ import Footer from "./components/Footer"
 import "./index.css"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path = "/">
-          <HomePage />
-        </Route>
-        <Route
-          path = "/sign-in">
-          <SignInPage />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <React.StrictMode>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path = "/">
+            <HomePage />
+          </Route>
+          <Route
+            path = "/sign-in">
+            <SignInPage />
+          </Route>
+          <Route
+            path = "/user">
+            <UserPage />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 )
