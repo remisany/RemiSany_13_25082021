f17f749ce6a2fc4c6463715a2add1ce710c9de24
remisany <remi.d.sany@gmail.com>
2021-09-02 15:48:47 +0200

    Ajout soumission formulaire

--- argent-bank/src/components/Form.jsx
+++ argent-bank/src/components/Form.jsx
@@ -1,14 +1,17 @@
-//React
-import { useDispatch } from 'react-redux'
+import { Fragment, useState } from "react"
+import { Redirect } from "react-router-dom"
+import { useStore, useDispatch, useSelector } from "react-redux"
+import styled from "styled-components"
+import axios from "axios"
 
 //Components
 import Input from "./Input"
 
-//Styled components
-import styled from "styled-components"
-
 //Features
-import { handleSubmit } from '../features/SignIn'
+import { valid } from '../features/LogIn'
+
+//Selectors
+import { selectToken } from "../utils/selectors"
 
 const BUTTON = styled.button`
     display: block;
@@ -22,25 +25,57 @@ const BUTTON = styled.button`
     color: #fff;
 `
 
+const SPAN = styled.span`
+    display: block;
+    font-size: 12px;
+    padding-top: 10px;
+    color: #f00;
+`
+
 function Form () {
+    const store = useStore()
     const dispatch = useDispatch()
+    const token = useSelector(selectToken)
+    const [errorMessage, setErrorMessage] = useState("")
+    
+    const login = (e) => {
+        e.preventDefault()
+
+        const user = store.getState().signIn
+
+        return axios
+        .post("http://localhost:3001/api/v1/user/login", user)
+        .then((response) => {
+            if (response.data.body.token) {
+                dispatch(valid(response.data.body.token))
+            }
+        })
+        .catch((e) => {
+            setErrorMessage(e.response.data.message)
+        })
+    }
 
     return (
-        <form onSubmit = {(e) => dispatch(handleSubmit(e))}>
-            <Input
-                type = "text"
-                id = "username"
-            />
-            <Input
-                type = "password"
-                id = "password"
-            />
-            <Input
-                type = "checkbox"
-                id = "remember-me"
-            />
-            <BUTTON>Sign In</BUTTON>
-        </form>
+        <Fragment>
+            {token !=="" && <Redirect to = "/profile" />}
+
+            <form onSubmit = {(e) => login(e)}>
+                <Input
+                    type = "text"
+                    id = "username"
+                />
+                <Input
+                    type = "password"
+                    id = "password"
+                />
+                <Input
+                    type = "checkbox"
+                    id = "remember-me"
+                />
+                <BUTTON>Sign In</BUTTON>
+                {token === "" && <SPAN>{errorMessage}</SPAN>}
+            </form>
+        </Fragment>
     )
 }
 
