78a8f8ef27563a73ecfca2ea2a33ce16c68c0d12
remisany <remi.d.sany@gmail.com>
2021-09-02 15:51:54 +0200

    AJout nom utilisateur

--- argent-bank/src/components/Welcome.jsx
+++ argent-bank/src/components/Welcome.jsx
@@ -1,5 +1,8 @@
-//Styled components
+import { useSelector } from "react-redux"
 import styled from "styled-components"
+
+//Selectors
+import { selectFirstname, selectLastname } from "../utils/selectors"
 
 const HEADER = styled.div`
     color: #fff;
@@ -15,9 +18,12 @@ const BUTTON = styled.button`
 `
 
 function Welcome () {
+    const firstname = useSelector(selectFirstname)
+    const lastname = useSelector(selectLastname)
+
     return (
         <HEADER>
-            <h1>Welcome back</h1>
+            <h1>Welcome back<br />{firstname} {lastname}!</h1>
             <BUTTON>Edit Name</BUTTON>
         </HEADER>
     )
